803f1cd882e85db3ae9209099f91700134269945
Aditi Bora <aditi.bora2005@gmail.com>
2026-05-06 12:43:00 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+       int i=0;
+       while(nums.size() > 0 && i<nums.size()-1){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+            i++;
+       }
+
+
+       return false;
+    }
+};
