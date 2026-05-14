56066e49675acd4b0ce21a87cf4f0ee6d80ec474
Aditi Bora <aditi.bora2005@gmail.com>
2026-05-14 20:13:39 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        
+        int start=0;
+        int end=nums.size();
+        int mid=(start+end)/2 ;
+
+        int ans=nums[mid];
+        return ans;
+
+        
+         
+    }
+};
